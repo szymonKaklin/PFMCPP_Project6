3dbfba20ae980943f1ccbe1f9a9798923539312e
szymonKaklin <szymon.kaklin@hotmail.co.uk>
2020-10-04 23:42:14 +0000

    Completed Part1

--- main.cpp
+++ main.cpp
@@ -1,45 +1,45 @@
 /*
  Project 6: Part 1 / 2
  Video: Chapter 3 Part 2
-
-Create a branch named Part1
-
-Purpose:  This project will show you the difference between member functions and static class functions, as well as the difference between pointers and references and the code semantics that accompany pointers and references.
-
+ 
+ Create a branch named Part1
+ 
+ Purpose:  This project will show you the difference between member functions and static class functions, as well as the difference between pointers and references and the code semantics that accompany pointers and references.
+ 
  Pointers
-
+ 
  Please look at the screenshot in the files to see what happens if you paste this assignment into an existing cpp file in an xcode project
  
  Task: replace all of the <place holder> with proper names and types that will make this program compile and run.
  
  press the Compile, link and run... button and work your way through all of the error messages.
  
-
+ 
  steps:
  
  1) finish implementing the constructor for T. the constructor parameters need to initialize 2) and 3)
  
  2) provide a member variable in T of a type of your choosing that works with the compare function below T
-         note the name of the variable used in the compare function
+ note the name of the variable used in the compare function
  
  3) provide a member variable of type std::string in T that works with the *first* cout statement in main().
-         note the name of the variable used in the *first* cout statement in main()
+ note the name of the variable used in the *first* cout statement in main()
  
  4) finish defining the struct with the compare function
  5) provide proper types for the arguments to compare, and a proper return value
-         be sure to make it work with the pointer semantics being used in the if statements.
-         see 8) for a hint about what type to use.
+ be sure to make it work with the pointer semantics being used in the if statements.
+ see 8) for a hint about what type to use.
  
  6) finish instantiating the two T instances being declared in main(), based on the constructor you finished implementing in T
-     Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
-     Don't let this confuse you.
+ Their names (line 105,106) are different than the member variables names (line 77), even tho the placeholders share the same name.
+ Don't let this confuse you.
  
  7) Declare 'f' with the proper type so the call to f.compare works
  
  8) call f.compare() with the correct arguments, based on 2) and 5)
  
  9) correct the cout that uses smaller's member variable with some safe pointer usage.
-         the possible return values of the compare() function should give you a hint.
+ the possible return values of the compare() function should give you a hint.
  
  10) complete the implementation for the static function in <structName2>
  
@@ -53,10 +53,10 @@ Purpose:  This project will show you the difference between member functions and
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
-Make a pull request after you make your first commit and pin the pull request link to our DM thread.
-
-send me a DM to check your pull request
-
+ Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+ 
+ send me a DM to check your pull request
+ 
  Wait for my code review.
  */
 
@@ -64,14 +64,19 @@ send me a DM to check your pull request
 #include <string>
 struct T
 {
-    T(<#type name#> v, const char* <#variable name#>)   //1
-    //2
-    //3
+    T(int v, const char x)
+    {
+        value = v;
+        name = x;
+    } //1
+    int value; //2
+    std::string name; //3
 };
 
-struct <#structName1#>                                //4
+struct Comparator                            //4
 {
-    <#type name#> compare(<#type name#> a, <#type name#> b) //5
+    Comparator() {}
+    T* compare(T* a, T* b) //5
     {
         if( a->value < b->value ) return a;
         if( a->value > b->value ) return b;
@@ -81,49 +86,82 @@ struct <#structName1#>                                //4
 
 struct U
 {
-    float <#name1#> { 0 }, <#name2#> { 0 };
-    <#returnType#> <#memberFunction#>(<#type name#>* <#updatedValue#>)      //12
+    float first { 4.5f }, second { 8.23f };
+    float memberFunction(float* newValue)      //12
     {
-        
+        std::cout << "U's first value: " << first << std::endl;
+        if( newValue != nullptr )
+        {
+            first = *newValue;
+        }
+        std::cout << "U's first updated value: " << first << std::endl;
+        while( std::abs(second - first) > 0.001f )
+        {
+            if( second > first)
+            {
+                first += 0.001f;
+            }
+            else
+            {
+                second += 0.001f;
+            }
+        }
+        std::cout << "U's second updated value: " << second << std::endl;
+        return second * first;
     }
 };
 
-struct <#structname2#>
+struct structTwo
 {
-    static <#returntype#> <#staticFunctionA#>(U* that, <#type name#>* <#updatedValue#> )        //10
+    static float staticFunctionA(U* that, float* newValue )        //10
     {
-        std::cout << "U's <#name1#> value: " << that-><#name1#> << std::endl;
-        that-><#name1#> = <#updatedValue#>;
-        std::cout << "U's <#name1#> updated value: " << that-><#name1#> << std::endl;
-        while( std::abs(that-><#name2#> - that-><#name1#>) > 0.001f )
+        std::cout << "U's first value: " << that->first << std::endl;
+        that->first = *newValue;
+        std::cout << "U's first updated value: " << that->first << std::endl;
+        while( std::abs(that->second - that->first) > 0.001f )
         {
             /*
-             write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
+             write something that makes the distance between that->second and that->first get smaller
              */
-            that-><#name2#> += ;
+            if( that->second > that->first)
+            {
+                that->first += 0.001f;
+            }
+            else
+            {
+                that->second += 0.001f;
+            }
         }
-        std::cout << "U's <#name2#> updated value: " << that-><#name2#> << std::endl;
-        return that-><#name2#> * that-><#name1#>;
+        std::cout << "U's second updated value: " << that->second << std::endl;
+        return that->second * that->first;
     }
 };
         
 int main()
 {
-    T <#name1#>( , );                                             //6
-    T <#name2#>( , );                                             //6
+    T first(5, 'f');                                            //6
+    T second(7, 's');                                             //6
     
-    <#structName1#> f;                                            //7
-    auto* smaller = f.compare( , );                              //8
-    std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+    Comparator f;                                            //7
+    auto* smaller = f.compare(&first, &second);                              //8
+    if( smaller != nullptr )
+    {
+        std::cout << "the smaller one is << " << smaller->name << std::endl; //9
+    }
+    else
+    {
+        std::cout << "both are the same" << std::endl;
+    }
     
-    U <#name3#>;
+    
+    U thirdTest;
     float updatedValue = 5.f;
-    std::cout << "[static func] <#name3#>'s multiplied values: " << <#structname2#>::<#staticFunctionA#>( , ) << std::endl;                  //11
+    std::cout << "[static func] thirdTest's multiplied values: " << structTwo::staticFunctionA( &thirdTest, &updatedValue ) << std::endl;                  //11
     
-    U <#name4#>;
-    std::cout << "[member func] <#name4#>'s multiplied values: " << <#name4#>.<#memberFunction#>( &updatedValue ) << std::endl;
+    U fourthTest;
+    std::cout << "[member func] fourthTest's multiplied values: " << fourthTest.memberFunction( &updatedValue ) << std::endl;
 }
-
+        
         
         
         
